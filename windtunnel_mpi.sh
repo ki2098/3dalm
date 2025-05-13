01aada6c875325eea067eafd070fba6d754a4872
ki2098 <1336670149@qq.com>
2025-05-13 16:17:24 +0900

    backup

--- windtunnel_mpi.sh
+++ windtunnel_mpi.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 #PJM -L rscgrp=b-batch
-#PJM -L node=1
-#PJM --mpi proc=4
-#PJM -L elapse=1:00:00
+#PJM -L gpu=1
+#PJM --mpi proc=1
+#PJM -L elapse=3:00:00
 #PJM -j
 #PJM -o windtunnel_mpi.log
 
 module load nvidia
 module load nvompi
-
-echo $PJM_MPI_PROCS
-echo $PJM_PROC_BY_NODE
 
 mpiexec -n 1 -display-map bin/windtunnel_mpi plain_windtunnel_gk.json
