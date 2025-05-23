8784be369ec8524dd6c6d4d6be96f7e647c90f9c
ki2098 <1336670149@qq.com>
2025-05-23 18:52:29 +0900

    adjust convertmesh

--- script/convertmesh.py
+++ script/convertmesh.py
@@ -9,57 +9,30 @@ gc = 2
 
 os.makedirs(os.path.dirname(dst_path), exist_ok=True)
 
-with open(f'{src_dir}/x.txt') as f:
-    nx = int(f.readline())
-    xlist = []
-    for line in f:
-        xlist.append(float(line))
+def build_coordinate(path):
+    with open(path) as f:
+        nx = int(f.readline())
+        xlist = []
+        for line in f:
+            xlist.append(float(line))
 
-with open(f'{src_dir}/y.txt') as f:
-    ny = int(f.readline())
-    ylist = []
-    for line in f:
-        ylist.append(float(line))
+        cx = nx - 1 + 2*gc
+        x = [0.]*cx
 
-with open(f'{src_dir}/z.txt') as f:
-    nz = int(f.readline())
-    zlist = []
-    for line in f:
-        zlist.append(float(line))
+        for i in range(gc, cx - gc):
+            x[i]  = (xlist[i - gc] + xlist[i - gc + 1])/2
 
-cx = nx - 1 + 2*gc
-cy = ny - 1 + 2*gc
-cz = nz - 1 + 2*gc
-x = [0.]*cx
-y = [0.]*cy
-z = [0.]*cz
+        for i in range(gc - 1, -1, -1):
+            x[i] = 3*x[i + 1] - 3*x[i + 2] + x[i + 3]
 
-for i in range(gc, cx - gc):
-    x[i]  = (xlist[i - gc] + xlist[i - gc + 1])/2
+        for i in range(cx - gc, cx):
+            x[i] = 3*x[i - 1] - 3*x[i - 2] + x[i - 3]
 
-for i in range(gc - 1, -1, -1):
-    x[i] = 3*x[i + 1] - 3*x[i + 2] + x[i + 3]
+        return cx, x
 
-for i in range(cx - gc, cx):
-    x[i] = 3*x[i - 1] - 3*x[i - 2] + x[i - 3]
-
-for j in range(gc, cy - gc):
-    y[j]  = (ylist[j - gc] + ylist[j - gc + 1])/2
-
-for j in range(gc - 1, -1, -1):
-    y[j] = 3*y[j + 1] - 3*y[j + 2] + y[j + 3]
-
-for j in range(cy - gc, cy):
-    y[j] = 3*y[j - 1] - 3*y[j - 2] + y[j - 3]
-
-for k in range(gc, cz - gc):
-    z[k]  = (zlist[k - gc] + zlist[k - gc + 1])/2
-
-for k in range(gc - 1, -1, -1):
-    z[k] = 3*z[k + 1] - 3*z[k + 2] + z[k + 3]
-
-for k in range(cz - gc, cz):
-    z[k] = 3*z[k - 1] - 3*z[k - 2] + z[k - 3]
+cx, x = build_coordinate(f'{src_dir}/x.txt')
+cy, y = build_coordinate(f'{src_dir}/y.txt')
+cz, z = build_coordinate(f'{src_dir}/z.txt')
 
 with open(dst_path, "w") as f:
     f.write(f'{cx} {cy} {cz} {gc}\n')
