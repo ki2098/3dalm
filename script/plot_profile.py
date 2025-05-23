fe9b938c01dc4242ff7b50ad05a781be84b1636f
ki2098 <1336670149@qq.com>
2025-05-23 17:42:19 +0900

    backup

---
+++ script/plot_profile.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+import glob
+import sys
+
+prefix = sys.argv[1]
+
+files = glob.glob(prefix + "*")
+
+for f in files:
+    csv = pd.read_csv(f)
+    x = csv['Points:0'][0]
+    plt.plot(csv['U:0'], csv['Points:2'], label=f'x={x}')
+
+plt.grid(True)
+plt.legend()
+plt.xlabel('u/Uin')
+plt.ylabel('z/R')
+plt.show()
