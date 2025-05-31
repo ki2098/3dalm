4a171492a755d51a872c90be656d4a086124d425
ki2098 <1336670149@qq.com>
2025-05-31 13:39:29 +0900

    backup

--- cases/plain_windtunnel_20/windtunnel.sh
+++ cases/plain_windtunnel_20/windtunnel.sh
@@ -3,11 +3,15 @@
 #PJM -L rscgrp=b-batch
 #PJM -L node=1
 #PJM --mpi proc=4
-#PJM -L elapse=3:00:00
+#PJM -L elapse=12:00:00
 #PJM -j
 #PJM -o run.log
 
 module load nvidia
 module load nvompi
 
+date
+
 mpiexec -n 4 -display-map ../../bin/windtunnel --clear .
+
+date
